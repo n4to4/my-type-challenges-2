88d362cd9c21ee2146a8f3f11d38d215eab79e29
n4to4 <n4to4k@gmail.com>
2024-12-28 06:55:00 +0900

    wip

--- 00898-easy-includes.ts
+++ 00898-easy-includes.ts
@@ -25,4 +25,11 @@ type cases = [
 ];
 
 // ============= Your Code Here =============
-type Includes<T extends readonly any[], U> = any;
+type Includes<T extends readonly any[], U> = T extends [
+  infer Head,
+  ...infer Tail
+]
+  ? Equal<Head, U> extends true
+    ? true
+    : Includes<Tail, U>
+  : false;
