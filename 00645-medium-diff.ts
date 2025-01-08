791792e9cf4f2e0ce7a83ad6367bfa5f049177a7
n4to4 <n4to4k@gmail.com>
2025-01-09 07:59:13 +0900

    wip

---
+++ 00645-medium-diff.ts
@@ -0,0 +1,28 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type Foo = {
+  name: string;
+  age: string;
+};
+type Bar = {
+  name: string;
+  age: string;
+  gender: number;
+};
+type Coo = {
+  name: string;
+  gender: number;
+};
+
+type cases = [
+  Expect<Equal<Diff<Foo, Bar>, { gender: number }>>,
+  Expect<Equal<Diff<Bar, Foo>, { gender: number }>>,
+  Expect<Equal<Diff<Foo, Coo>, { age: string; gender: number }>>,
+  Expect<Equal<Diff<Coo, Foo>, { age: string; gender: number }>>
+];
+
+// ============= Your Code Here =============
+type Diff<O1, O2> = Omit<Omit<O1, keyof O2> & Omit<O2, keyof O1>, never>;
+
+type X1 = Diff<Foo, Bar>;
