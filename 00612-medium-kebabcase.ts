6fbfa65a0f59b59d37c985f7c440fe8b326a6da8
n4to4 <n4to4k@gmail.com>
2025-01-08 09:25:38 +0900

    wip

--- 00612-medium-kebabcase.ts
+++ 00612-medium-kebabcase.ts
@@ -14,6 +14,7 @@ type cases = [
 ];
 
 // ============= Your Code Here =============
+/*
 type Upper = StringToUnion<"ABCDEFGHIJKLMNOPQRSTUVWXYZ">;
 type StringToUnion<T extends string> = T extends `${infer Head}${infer Tail}`
   ? Head | StringToUnion<Tail>
@@ -29,5 +30,13 @@ type KebabCase<
   : T extends `${infer Head1}${infer Tail2}`
   ? `${Head1}${KebabCase<Tail2, false>}`
   : "";
+*/
+
+type KebabCase<S extends string> = S extends `${infer S1}${infer S2}`
+  ? S2 extends Uncapitalize<S2>
+    ? `${Uncapitalize<S1>}${KebabCase<S2>}`
+    : `${Uncapitalize<S1>}-${KebabCase<S2>}`
+  : S;
 
 type X1 = KebabCase<"AbcFooBar">;
+type X2 = Uncapitalize<"ABCDEF">;
