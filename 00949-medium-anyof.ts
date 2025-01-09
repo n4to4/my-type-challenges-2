9d290bd7fef7bee05fb5494d13ed8fbd43c614b0
n4to4 <n4to4k@gmail.com>
2025-01-10 07:19:45 +0900

    wip

--- 00949-medium-anyof.ts
+++ 00949-medium-anyof.ts
@@ -20,13 +20,19 @@ type cases = [
 
 // ============= Your Code Here =============
 type Falsy = 0 | "" | false | undefined | null | [];
-type AnyOf<T extends readonly any[]> = T extends [infer Head, ...infer Tail]
+type AnyOf1<T extends readonly any[]> = T extends [infer Head, ...infer Tail]
   ? Head extends Falsy
-    ? AnyOf<Tail>
+    ? AnyOf1<Tail>
     : keyof Head extends never
-    ? AnyOf<Tail>
+    ? AnyOf1<Tail>
     : true
   : false;
+
+type AnyOf<T extends readonly any[]> = T[number] extends
+  | Falsy
+  | { [key: string]: never }
+  ? false
+  : true;
 
 type X1 = AnyOf<[0, "", false, {}, undefined, null]>;
 type EmptyObject<T> = keyof T extends never ? 1 : 2;
