ce176b713b32ee7673074f4a4f03136a5158d7c2
n4to4 <n4to4k@gmail.com>
2025-01-23 07:00:28 +0900

    wip

--- 04260-medium-nomiwase.ts
+++ 04260-medium-nomiwase.ts
@@ -99,4 +99,16 @@ type cases = [
 ];
 
 // ============= Your Code Here =============
-type AllCombinations<S> = any;
+type String2Union<S extends string> = S extends `${infer C}${infer REST}`
+  ? C | String2Union<REST>
+  : never;
+
+type AllCombinations<
+  STR extends string,
+  S extends string = String2Union<STR>
+> = [S] extends [never]
+  ? ""
+  : "" | { [K in S]: `${K}${AllCombinations<never, Exclude<S, K>>}` }[S];
+
+type X1 = AllCombinations<"AB">;
+type X2 = AllCombinations<"ABC">;
