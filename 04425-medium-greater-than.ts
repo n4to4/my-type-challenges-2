02671d6746cc1615b4101ec1f298961ee110968c
n4to4 <n4to4k@gmail.com>
2025-01-23 07:09:37 +0900

    wip

--- 04425-medium-greater-than.ts
+++ 04425-medium-greater-than.ts
@@ -14,7 +14,7 @@ type cases = [
 ];
 
 // ============= Your Code Here =============
-type GreaterThan<T extends number, U extends number> = T extends U
+type GreaterThan1<T extends number, U extends number> = T extends U
   ? false
   : GTHelper<T, U>;
 type GTHelper<
@@ -27,3 +27,38 @@ type GTHelper<
   : UI["length"] extends U
   ? true
   : GTHelper<T, U, [...TI, 0], [...UI, 0]>;
+
+type ParseInt<T> = T extends `${infer X extends number}` ? X : never;
+
+type RemoveLeadingZeros<T extends string> = T extends "0"
+  ? T
+  : T extends `${0}${infer Rest}`
+  ? RemoveLeadingZeros<Rest>
+  : T;
+
+type InnerMinusOne<T extends string> =
+  T extends `${infer X extends number}${infer Y}`
+    ? X extends 0
+      ? `9${InnerMinusOne<Y>}`
+      : `${[-1, 0, 1, 2, 3, 4, 5, 6, 7, 8][X]}${Y}`
+    : "";
+
+type Reverse<T extends string> = T extends `${infer X}${infer Y}`
+  ? `${Reverse<Y>}${X}`
+  : "";
+
+type MinusOne<T extends number> = ParseInt<
+  RemoveLeadingZeros<Reverse<InnerMinusOne<Reverse<`${T}`>>>>
+>;
+
+type InnerGreaterThan<T extends number, U extends number> = T extends U
+  ? true
+  : T extends 0
+  ? false
+  : InnerGreaterThan<MinusOne<T>, U>;
+
+type GreaterThan<T extends number, U extends number> = T extends U
+  ? false
+  : U extends 0
+  ? true
+  : InnerGreaterThan<T, U>;
