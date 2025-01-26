6f9f623ba9fd02f7c25393dc6a9d6af982259001
n4to4 <n4to4k@gmail.com>
2025-01-27 08:02:09 +0900

    type-challenges

---
+++ 05117-medium-without.ts
@@ -0,0 +1,11 @@
+// ============= Test Cases =============
+import type { Equal, Expect } from "./test-utils";
+
+type cases = [
+  Expect<Equal<Without<[1, 2], 1>, [2]>>,
+  Expect<Equal<Without<[1, 2, 4, 1, 5], [1, 2]>, [4, 5]>>,
+  Expect<Equal<Without<[2, 3, 2, 3, 2, 3, 2, 3], [2, 3]>, []>>
+];
+
+// ============= Your Code Here =============
+type Without<T, U> = any;
