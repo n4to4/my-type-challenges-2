04d69ad0de1373421baabd94142416c66b4f138a
n4to4 <n4to4k@gmail.com>
2025-02-03 07:10:14 +0900

    type-challenges

--- 09286-medium-firstuniquecharindex.ts
+++ 09286-medium-firstuniquecharindex.ts
@@ -10,4 +10,35 @@ type cases = [
 ];
 
 // ============= Your Code Here =============
-type FirstUniqueCharIndex<T extends string> = any;
+// type Repeating<
+//   T extends string,
+//   Rest extends string,
+//   Seen,
+//   SeenStatus = T extends Seen ? true : false,
+//   Repeating = Rest extends `${string}${T}${string}` ? true : false
+// > = SeenStatus extends true ? (Repeating extends true ? true : false) : false;
+
+// type FirstUniqueCharIndex<
+//   T extends string,
+//   Idx extends any[] = [],
+//   Seen = never
+// > = T extends `${infer First}${infer Rest}`
+//   ? Repeating<First, Rest, Seen> extends true
+//     ? FirstUniqueCharIndex<Rest, [...Idx, any], Seen | First>
+//     : Idx["length"]
+//   : -1;
+
+type FirstUniqueCharIndex<
+  T extends string,
+  _Acc extends string[] = []
+> = T extends ""
+  ? -1
+  : T extends `${infer Head}${infer Rest}`
+  ? Head extends _Acc[number]
+    ? FirstUniqueCharIndex<Rest, [..._Acc, Head]>
+    : Rest extends `${string}${Head}${string}`
+    ? FirstUniqueCharIndex<Rest, [..._Acc, Head]>
+    : _Acc["length"]
+  : never;
+
+type X1 = FirstUniqueCharIndex<"aabb">;
