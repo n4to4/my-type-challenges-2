ec34b1d717ba8ce5c7b69ed82373cab531b842bb
n4to4 <n4to4k@gmail.com>
2025-02-04 07:47:44 +0900

    type-challenges

--- 09616-medium-parse-url-params.ts
+++ 09616-medium-parse-url-params.ts
@@ -11,16 +11,21 @@ type cases = [
 ];
 
 // ============= Your Code Here =============
-type Split<T extends string> = T extends `${infer Head}/${infer Rest}`
-  ? Head | Split<Rest>
-  : T;
+// type Split<T extends string> = T extends `${infer Head}/${infer Rest}`
+//   ? Head | Split<Rest>
+//   : T;
 
-type ParseUrlParams<T extends string> = T extends "" ? never : Helper<T>;
+// type ParseUrlParams<T extends string> = T extends "" ? never : Helper<T>;
 
-type Helper<T extends string, S = Split<T>> = S extends `:${infer P}`
-  ? P
-  : never;
+// type Helper<T extends string, S = Split<T>> = S extends `:${infer P}`
+//   ? P
+//   : never;
 
-type X0 = Split<"posts/:id/:user">;
-type X1 = Split<"">;
+// type X0 = Split<"posts/:id/:user">;
+// type X1 = Split<"">;
 type X2 = ParseUrlParams<"posts/:id/:user">;
+
+type GetPathParam<T extends string> = T extends `:${infer P}` ? P : never;
+type ParseUrlParams<T extends string> = T extends `${infer L}/${infer R}`
+  ? GetPathParam<L> | ParseUrlParams<R>
+  : GetPathParam<T>;
