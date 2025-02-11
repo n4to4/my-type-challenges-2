1dafafde4e586b861deb52468b10ed4b3717a975
n4to4 <n4to4k@gmail.com>
2025-02-12 07:00:08 +0900

    type-challenges

--- 09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su.ts
+++ 09898-medium-zhao-chu-mu-biao-shu-zu-zhong-zhi-chu-xian-guo-yi-ci-de-yuan-su.ts
@@ -1,6 +1,5 @@
 // ============= Test Cases =============
 import type { Equal, Expect } from "./test-utils";
-import { ExpectFalse, NotEqual } from "./test-utils";
 
 type cases = [
   Expect<Equal<FindEles<[1, 2, 2, 3, 3, 4, 5, 6, 6, 6]>, [1, 4, 5]>>,
@@ -9,4 +8,13 @@ type cases = [
 ];
 
 // ============= Your Code Here =============
-type FindEles<T extends any[]> = any;
+type FindEles<T extends any[], Duplicates = never> = T extends [
+  infer F,
+  ...infer R
+]
+  ? F extends Duplicates
+    ? FindEles<R, Duplicates>
+    : F extends R[number]
+    ? FindEles<R, Duplicates | F>
+    : [F, ...FindEles<R, Duplicates>]
+  : [];
